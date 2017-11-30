04fa90709959f7eba053d9873cb0737a9b0bb08f
Aaron Chesley <FlashBanistan66@gmail.com>
2017-11-30 09:24:00 -0800

    redo SyndicationFeed

--- xml_tutorial/feeds.py
+++ xml_tutorial/feeds.py
@@ -1,5 +1,5 @@
 from django.contrib.syndication.views import Feed
-from django.utils.feedgenerator import SyndicationFeed, rfc2822_date
+from django.utils.feedgenerator import rfc2822_date
 from django.utils.xmlutils import SimplerXMLGenerator
 import datetime
 from django.utils.timezone import utc
@@ -7,41 +7,98 @@ from django.utils.encoding import iri_to_uri
 from jobs.models import Job
 
 
-class IndeedFeed(SyndicationFeed):
+class IndeedFeed:
+    "Base class for all syndication feeds. Subclasses should provide write()"
+    def __init__(self, title, date, referencenumber, url, company, sourcename, country, email, description, **kwargs):
+        def to_str(s):
+            return str(s) if s is not None else s
 
-    content_type = 'application/rss+xml; charset=utf-8'
+        self.feed = {
+            'title': to_str(title),
+            'date': to_str(date),
+            'referencenumber': to_str(referencenumber),
+            'url': iri_to_uri(url),
+            'company': to_str(company),
+            'sourcename': to_str(sourcename),
+            'country': to_str(country),
+            'email': to_str(email),
+            'description': to_str(description),
+        }
+        self.feed.update(kwargs)
+        self.items = []
 
-    def write(self, outfile, encoding):
-        handler = SimplerXMLGenerator(outfile, encoding)
-        handler.startDocument()
-        handler.startElement("source", {})
-        self.add_root_elements(handler)
-        self.write_items(handler)
-        handler.endElement("source")
+    def add_item(self, title, date, referencenumber, url, company, sourcename, country, email, description, **kwargs):
+        """
+        Add an item to the feed. All args are expected to be strings except
+        pubdate and updateddate, which are datetime.datetime objects, and
+        enclosures, which is an iterable of instances of the Enclosure class.
+        """
+        def to_str(s):
+            return str(s) if s is not None else s
+        item = {
+            'title': to_str(title),
+            'date': to_str(date),
+            'referencenumber': to_str(referencenumber),
+            'url': iri_to_uri(url),
+            'company': to_str(company),
+            'sourcename': to_str(sourcename),
+            'country': to_str(country),
+            'email': to_str(email),
+            'description': to_str(description),
+        }
+        item.update(kwargs)
+        self.items.append(item)
+
+    def num_items(self):
+        return len(self.items)
+
+    def root_attributes(self):
+        """
+        Return extra attributes to place on the root (i.e. feed/channel) element.
+        Called from write().
+        """
+        return {}
 
     def add_root_elements(self, handler):
-        super(IndeedFeed, self).add_root_elements(handler)
-        handler.addQuickElement('publisher', 'ATS Name')
-        handler.addQuickElement('publisherUrl', 'http://www.atssite.com')
-        handler.addQuickElement("lastBuildDate", rfc2822_date(self.latest_post_date()))
+        """
+        Add elements in the root (i.e. feed/channel) element. Called
+        from write().
+        """
+        pass
 
-    def write_items(self, handler):
-        for item in self.items:
-            handler.startElement('job', self.item_attributes(item))
-            self.add_item_elements(handler, item)
-            handler.endElement("job")
+    def item_attributes(self, item):
+        """
+        Return extra attributes to place on each item (i.e. item/entry) element.
+        """
+        return {}
 
     def add_item_elements(self, handler, item):
-        super(IndeedFeed, self).add_item_elements(handler, item)
-        handler.addQuickElement(u"title", item['title'])
-        handler.addQuickElement("date", item['date'])
-        handler.addQuickElement("referencenumber", item['referencenumber'])
-        handler.addQuickElement("url", item['link'])
-        handler.addQuickElement("company", item['company'])
-        handler.addQuickElement("sourcename", item['sourcename'])
-        handler.addQuickElement("country", item['country'])
-        handler.addQuickElement("email", item['email'])
-        handler.addQuickElement("description", item['description'])
+        """
+        Add elements on each item (i.e. item/entry) element.
+        """
+        pass
+
+    def write(self, outfile, encoding):
+        """
+        Output the feed in the given encoding to outfile, which is a file-like
+        object. Subclasses should override this.
+        """
+        raise NotImplementedError('subclasses of SyndicationFeed must provide a write() method')
+
+    def writeString(self, encoding):
+        """
+        Return the feed in the given encoding as a string.
+        """
+        s = StringIO()
+        self.write(s, encoding)
+        return s.getvalue()
+
+    def latest_post_date(self):
+        return datetime.datetime.utcnow().replace(tzinfo=utc)
+
+
+
+
 
 
 class JobsFeed(Feed):
@@ -52,25 +109,7 @@ class JobsFeed(Feed):
     def items(self):
         return Job.objects.all()
 
-    def item_title(self, item):
-        return item.title
+    
 
-    def item_description(self, item):
-        return item.description
-
-    def item_extra_kwargs(self, obj):
-        """
-        Returns an extra keyword arguments dictionary that is used with
-        the `add_item` call of the feed generator.
-        Add the 'content' field of the 'Entry' item, to be used by the custom feed generator.
-        """
-        return {
-            'date': str(obj.date),
-            'referencenumber': obj.referencenumber,
-            'company': obj.company,
-            'sourcename': obj.sourcename,
-            'country': obj.country,
-            'email': obj.email    
-        }
 
   
