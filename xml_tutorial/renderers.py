90c909dc0f7f92eb637b6e64238f6dccd2edd713
Aaron Chesley <FlashBanistan66@gmail.com>
2017-12-01 09:55:43 -0800

    add comments

--- xml_tutorial/renderers.py
+++ xml_tutorial/renderers.py
@@ -17,14 +17,20 @@ class XMLRenderer(BaseRenderer):
 
     def render(self, data, accepted_media_type=None, renderer_context=None):
         """
-        Renders `data` into serialized XML.
+        Consider using the 'renderer_context' argument to set the 'publisher' and 'publisherurl' elements.
         """
+        print(dir(renderer_context['request']))
+        print(' ')
+        print(renderer_context['request'].data)
         # Helper variables
         root_tag_name = 'source'
         item_tag_name = 'job'
-        utf8_parser = E.XMLParser(encoding='utf-8')
-        # Start building xml:
+        # Build the root xml element:
         root = E.Element(root_tag_name)
+        # Add the 'publisher' element:
+        E.SubElement(root, 'publisher').text = 'ATS NAME'
+        # Add the 'publisherurl' element:
+        E.SubElement(root, 'publisherurl').text = 'http://www.atssite.com'
         # Unpack each dictionary from the data list:
         for d in data:
             # Create a job tag:
@@ -32,9 +38,9 @@ class XMLRenderer(BaseRenderer):
             # Loop through each ordered dictionary:
             for key, value in d.items():
                 # Add each key, value pair to an element and assign to job tag:
-                E.SubElement(job, key).text = value
+                E.SubElement(job, key).text = '<![CDATA[' + value + ']]>'
 
                 
-        print(E.tostring(root, pretty_print=True, xml_declaration=True, encoding='UTF-8'))
+        # print(E.tostring(root, pretty_print=True, xml_declaration=True, encoding='UTF-8'))
         # Pass the completed XML document to the view:
         return E.tostring(root, pretty_print=True, xml_declaration=True, encoding='UTF-8')
